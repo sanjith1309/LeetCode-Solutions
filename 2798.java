8349fae1d558d81f6721caa496f7feb2073d56e8
SANJITH KUMAR M <108807863+sanjith1309@users.noreply.github.com>
2023-07-30 14:45:22 +0530

    Update 2798.java

--- 2798.java
+++ 2798.java
@@ -1,1 +1,10 @@
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int met_target = 0;
+        for(int i=0;i<hours.length;i++){
+            if (hours[i]>=target){ met_target++; }
+        }
+        return met_target;
+    }
+}
 
